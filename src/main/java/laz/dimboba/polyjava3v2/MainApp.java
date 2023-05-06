d93988abbde7d467658bd2e675d383350aafb768
Dimboba <lazarkovdmitry@yandex.ru>
2023-05-07 02:53:22 +0300

    all UserDao functions

--- src/main/java/laz/dimboba/polyjava3v2/MainApp.java
+++ src/main/java/laz/dimboba/polyjava3v2/MainApp.java
@@ -5,16 +5,27 @@ import javafx.stage.Stage;
 import laz.dimboba.polyjava3v2.UserService.UserService;
 import laz.dimboba.polyjava3v2.entity.User;
 
+import java.util.List;
+
 public class MainApp extends Application {
 
     public void start(Stage mainStage) throws Exception{
         UserService userService = new UserService();
-        User user = userService.findUser(14);
-        System.out.println(user.toString());
-        System.out.println(user.getScore());
+//        User user = userService.findUser(14);
+//        System.out.println(user.toString());
+//        System.out.println(user.getScore());
+//
+//
+//        userService.saveUser("hello", "2345");
+//        user.setNickname("Abracadabra");
+//        userService.updateUser(user);
+//        userService.deleteUser(user);
 
 
-        userService.saveUser("hello", "2345");
+        List<User> users = userService.findAllUsers();
+        for(User user: users){
+            System.out.println(user.toString());
+        }
     }
 
     public static void main(String[] args){
--- src/main/java/laz/dimboba/polyjava3v2/dao/UserDaoImpl.java
+++ src/main/java/laz/dimboba/polyjava3v2/dao/UserDaoImpl.java
@@ -11,16 +11,13 @@ public class UserDaoImpl implements UserDao{
 
     @Override
     public User findById(int id) {
-        System.out.println(id + "!");
         try {
             Connection connection = new ConnectionFactory().getNewConnection();
             Statement statement = connection.createStatement();
-            System.out.println("here");
             String query = "Select * FROM users where id = " + id;
 
             ResultSet res = statement.executeQuery(query);
             res.next();
-            System.out.println(id + "!!!");
 
             String nickname = res.getString("nickname");
             String password = res.getString("password");
@@ -46,8 +43,10 @@ public class UserDaoImpl implements UserDao{
                     "VALUES (?, ?)";
             PreparedStatement preparedStatement =
                     connection.prepareStatement(query);
+
             preparedStatement.setString(1, nickname);
             preparedStatement.setString(2, passwd);
+
             ResultSet res = preparedStatement.executeQuery();
             connection.close();
         } catch (SQLException e){
@@ -59,14 +58,68 @@ public class UserDaoImpl implements UserDao{
     }
 
     public void update(User user) {
+        try {
+            Connection connection = new ConnectionFactory().getNewConnection();
+            String query = "UPDATE users " +
+                    "Set (nickname," +
+                    "password," +
+                    "score) =" +
+                    "(?,?,?)" +
+                    "where id=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
 
+            preparedStatement.setString(1, user.getNickname());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getScore());
+            preparedStatement.setInt(4, user.getId());
+
+            ResultSet res = preparedStatement.executeQuery();
+
+
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Update Error");
+            System.err.println(e.getMessage());
+            //TODO: что за ошибка??? При этом работает верно
+        }
     }
 
     public void delete(User user) {
-
+        try {
+            Connection connection = new ConnectionFactory().getNewConnection();
+            Statement statement = connection.createStatement();
+            String query = "Delete FROM users Where id = " + user.getId();
+            ResultSet res = statement.executeQuery(query);
+            connection.close();
+        } catch (SQLException e){
+            System.err.println("Delete Error");
+            System.err.println(e.getMessage());
+            //TODO: что за ошибка??? При этом работает верно
+        }
     }
 
     public List<User> findAllUsers() {
-        return new ArrayList<User>();
+        try {
+            Connection connection = new ConnectionFactory().getNewConnection();
+            Statement statement = connection.createStatement();
+            String query = "Select * FROM users order by score desc ";
+
+            ResultSet res = statement.executeQuery(query);
+            List<User> ans = new ArrayList<>();
+            while(res.next()) {
+                ans.add(new User(
+                   res.getInt("id"),
+                   res.getString("nickname"),
+                   res.getString("password"),
+                   res.getInt("score")
+                ));
+            }
+            connection.close();
+            return ans;
+        } catch(SQLException e) {
+            System.err.println("FindAll Error");
+            System.err.println((e.getMessage()));
+        }
+        return null;
     }
 }
